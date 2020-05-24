4f837fcf6913e2b92a97e194ccc11103b52ae24b
Yury Romanov <yury.romanov@bk.ru>
2020-05-24 18:01:24 +0300

    Add eslint and fix eslint problems

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./style.scss";
+import React from 'react'
+;
+import ReactDOM from 'react-dom';
+import './style.scss';
 
 const App = () => {
     return <h1>Welcome to the <strong>Financer</strong>!</h1>;
 };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
